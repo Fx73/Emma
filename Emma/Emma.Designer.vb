dbd8e61a2d4f431b42b2f9195b2f4f7ca64db805
fx <fx73000@yahoo.fr>
2018-12-20 17:53:59 +0100

    ok

---
+++ Emma/Emma.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Emma
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Emma))
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Button_Accept = New System.Windows.Forms.Button()
+        Me.Titre = New System.Windows.Forms.TextBox()
+        Me.Button_quit = New System.Windows.Forms.Button()
+        Me.MyTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Button_Compliment = New System.Windows.Forms.Button()
+        Me.Options = New System.Windows.Forms.CheckBox()
+        Me.Button_msg = New System.Windows.Forms.Button()
+        Me.CheckBoot = New System.Windows.Forms.CheckBox()
+        Me.TextEnvoi = New System.Windows.Forms.TextBox()
+        Me.TextRecu = New System.Windows.Forms.TextBox()
+        Me.Destinataire = New System.Windows.Forms.ComboBox()
+        Me.flager = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckCompliment = New System.Windows.Forms.CheckBox()
+        Me.interv_comp = New System.Windows.Forms.NumericUpDown()
+        Me.MDLserverpic = New System.Windows.Forms.PictureBox()
+        CType(Me.interv_comp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MDLserverpic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon.Text = "NotifyIcon"
+        Me.NotifyIcon.Visible = True
+        '
+        'Button_Accept
+        '
+        Me.Button_Accept.Location = New System.Drawing.Point(86, 93)
+        Me.Button_Accept.Name = "Button_Accept"
+        Me.Button_Accept.Size = New System.Drawing.Size(312, 23)
+        Me.Button_Accept.TabIndex = 0
+        Me.Button_Accept.Text = "Button_accept"
+        Me.Button_Accept.UseVisualStyleBackColor = True
+        '
+        'Titre
+        '
+        Me.Titre.BackColor = System.Drawing.Color.Crimson
+        Me.Titre.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Titre.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Titre.Font = New System.Drawing.Font("Arial Rounded MT Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Titre.ForeColor = System.Drawing.Color.Black
+        Me.Titre.Location = New System.Drawing.Point(7, 12)
+        Me.Titre.Multiline = True
+        Me.Titre.Name = "Titre"
+        Me.Titre.Size = New System.Drawing.Size(422, 49)
+        Me.Titre.TabIndex = 1
+        '
+        'Button_quit
+        '
+        Me.Button_quit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button_quit.Location = New System.Drawing.Point(86, 122)
+        Me.Button_quit.Name = "Button_quit"
+        Me.Button_quit.Size = New System.Drawing.Size(312, 23)
+        Me.Button_quit.TabIndex = 2
+        Me.Button_quit.Text = "Button_quit"
+        Me.Button_quit.UseVisualStyleBackColor = True
+        '
+        'MyTimer
+        '
+        Me.MyTimer.Enabled = True
+        Me.MyTimer.Interval = 20000
+        '
+        'Button_Compliment
+        '
+        Me.Button_Compliment.Location = New System.Drawing.Point(405, 93)
+        Me.Button_Compliment.Name = "Button_Compliment"
+        Me.Button_Compliment.Size = New System.Drawing.Size(24, 22)
+        Me.Button_Compliment.TabIndex = 3
+        Me.Button_Compliment.Text = "Compliment"
+        Me.Button_Compliment.UseVisualStyleBackColor = True
+        '
+        'Options
+        '
+        Me.Options.Appearance = System.Windows.Forms.Appearance.Button
+        Me.Options.AutoSize = True
+        Me.Options.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Options.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Options.FlatAppearance.CheckedBackColor = System.Drawing.Color.Green
+        Me.Options.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        Me.Options.Location = New System.Drawing.Point(404, 122)
+        Me.Options.Name = "Options"
+        Me.Options.Size = New System.Drawing.Size(53, 23)
+        Me.Options.TabIndex = 4
+        Me.Options.Text = "Options"
+        Me.Options.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Options.UseVisualStyleBackColor = False
+        '
+        'Button_msg
+        '
+        Me.Button_msg.Location = New System.Drawing.Point(7, 93)
+        Me.Button_msg.Name = "Button_msg"
+        Me.Button_msg.Size = New System.Drawing.Size(73, 52)
+        Me.Button_msg.TabIndex = 5
+        Me.Button_msg.Text = "Button_msg"
+        Me.Button_msg.UseVisualStyleBackColor = True
+        '
+        'CheckBoot
+        '
+        Me.CheckBoot.AutoSize = True
+        Me.CheckBoot.Location = New System.Drawing.Point(12, 235)
+        Me.CheckBoot.Name = "CheckBoot"
+        Me.CheckBoot.Size = New System.Drawing.Size(79, 17)
+        Me.CheckBoot.TabIndex = 6
+        Me.CheckBoot.Text = "CheckBoot"
+        Me.CheckBoot.UseVisualStyleBackColor = True
+        '
+        'TextEnvoi
+        '
+        Me.TextEnvoi.Location = New System.Drawing.Point(86, 67)
+        Me.TextEnvoi.Name = "TextEnvoi"
+        Me.TextEnvoi.Size = New System.Drawing.Size(342, 20)
+        Me.TextEnvoi.TabIndex = 7
+        '
+        'TextRecu
+        '
+        Me.TextRecu.Location = New System.Drawing.Point(102, 170)
+        Me.TextRecu.Multiline = True
+        Me.TextRecu.Name = "TextRecu"
+        Me.TextRecu.ReadOnly = True
+        Me.TextRecu.Size = New System.Drawing.Size(325, 40)
+        Me.TextRecu.TabIndex = 8
+        '
+        'Destinataire
+        '
+        Me.Destinataire.FormattingEnabled = True
+        Me.Destinataire.Items.AddRange(New Object() {"FX", "Lele Dav", "Emma", "Blandine"})
+        Me.Destinataire.Location = New System.Drawing.Point(7, 67)
+        Me.Destinataire.Name = "Destinataire"
+        Me.Destinataire.Size = New System.Drawing.Size(73, 21)
+        Me.Destinataire.TabIndex = 10
+        Me.Destinataire.Text = "Moi"
+        '
+        'flager
+        '
+        Me.flager.Enabled = True
+        Me.flager.Interval = 2000
+        '
+        'CheckCompliment
+        '
+        Me.CheckCompliment.AutoSize = True
+        Me.CheckCompliment.Location = New System.Drawing.Point(12, 214)
+        Me.CheckCompliment.Name = "CheckCompliment"
+        Me.CheckCompliment.Size = New System.Drawing.Size(166, 17)
+        Me.CheckCompliment.TabIndex = 11
+        Me.CheckCompliment.Text = "Compliments (toutes les x min)"
+        Me.CheckCompliment.UseVisualStyleBackColor = True
+        '
+        'interv_comp
+        '
+        Me.interv_comp.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+        Me.interv_comp.Location = New System.Drawing.Point(185, 214)
+        Me.interv_comp.Name = "interv_comp"
+        Me.interv_comp.Size = New System.Drawing.Size(39, 20)
+        Me.interv_comp.TabIndex = 12
+        Me.interv_comp.ThousandsSeparator = True
+        Me.interv_comp.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'MDLserverpic
+        '
+        Me.MDLserverpic.BackColor = System.Drawing.Color.Crimson
+        Me.MDLserverpic.BackgroundImage = Global.Emma.My.Resources.Resources.MDLserverKO
+        Me.MDLserverpic.ErrorImage = Global.Emma.My.Resources.Resources.MDLserverOK
+        Me.MDLserverpic.Image = Global.Emma.My.Resources.Resources.MDLserverKO
+        Me.MDLserverpic.InitialImage = Global.Emma.My.Resources.Resources.MDLserverLinked
+        Me.MDLserverpic.Location = New System.Drawing.Point(405, 13)
+        Me.MDLserverpic.Name = "MDLserverpic"
+        Me.MDLserverpic.Size = New System.Drawing.Size(18, 18)
+        Me.MDLserverpic.TabIndex = 13
+        Me.MDLserverpic.TabStop = False
+        '
+        'Emma
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BackgroundImage = Global.Emma.My.Resources.Resources.Fond
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(440, 260)
+        Me.Controls.Add(Me.MDLserverpic)
+        Me.Controls.Add(Me.interv_comp)
+        Me.Controls.Add(Me.CheckCompliment)
+        Me.Controls.Add(Me.Destinataire)
+        Me.Controls.Add(Me.TextRecu)
+        Me.Controls.Add(Me.TextEnvoi)
+        Me.Controls.Add(Me.CheckBoot)
+        Me.Controls.Add(Me.Button_msg)
+        Me.Controls.Add(Me.Options)
+        Me.Controls.Add(Me.Button_Compliment)
+        Me.Controls.Add(Me.Button_quit)
+        Me.Controls.Add(Me.Titre)
+        Me.Controls.Add(Me.Button_Accept)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Emma"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Emma"
+        CType(Me.interv_comp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MDLserverpic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NotifyIcon As NotifyIcon
+    Friend WithEvents Button_Accept As Button
+    Friend WithEvents Titre As TextBox
+    Friend WithEvents Button_quit As Button
+    Friend WithEvents MyTimer As Timer
+    Friend WithEvents Button_Compliment As Button
+    Friend WithEvents Options As CheckBox
+    Friend WithEvents Button_msg As Button
+    Friend WithEvents CheckBoot As CheckBox
+    Friend WithEvents TextEnvoi As TextBox
+    Friend WithEvents TextRecu As TextBox
+    Friend WithEvents Destinataire As ComboBox
+    Friend WithEvents flager As Timer
+    Friend WithEvents CheckCompliment As CheckBox
+    Friend WithEvents interv_comp As NumericUpDown
+    Friend WithEvents MDLserverpic As PictureBox
+End Class
---
+++ Emma/Emma.vb
@@ -0,0 +1,456 @@
+﻿'Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Imports System.IO
+Imports System.Drawing
+
+Public Class Emma
+
+
+    Dim user = Environment.UserName
+    Dim flag As String = "+w" + user
+    Const ERREUR As String = "Essaie de relancer l'app et contacte FX ou Leon"
+    Dim Mytooltip As New ToolTip
+
+    Dim compliment(1)
+    Dim nb_compliment As Integer
+    Dim shutup As Boolean = False
+
+    Dim Flux As NetworkStream
+    Dim FluxWriter As StreamWriter
+    Dim FluxReader As StreamReader
+    Public TCPClient As TcpClient = New TcpClient
+    'Public TCPListener As TcpListener = New TcpListener(IPAddress.Any, 11000)
+    Public ThreadReceive As System.Threading.Thread = New Threading.Thread(AddressOf Receive_message)
+    Public ThreadConnect As System.Threading.Thread = New Threading.Thread(AddressOf Connect_server)
+    Dim MdlIpAdress As IPAddress
+    Dim EtatConnexion As Integer = 0
+    '- 0 server introuvable
+    '- 1 trouvé mais TCP unlinked
+    '- 2 trouvé TCP en cours
+    '- 3 TCP linked mais Flux KO
+    '- 4 Connecté mmais Reception KO
+    '- 5 Connecté
+
+    Delegate Sub SetTextDelegate(ByVal TB As TextBox, ByVal sText As String)
+    Private SetText As New SetTextDelegate(AddressOf SetRecuTextBoxText)
+    Private Sub SetRecuTextBoxText(ByVal TB As TextBox, ByVal sText As String)
+        TB.Text = sText
+    End Sub
+
+
+    Private Sub Emma_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+                           Handles MyBase.Load
+        'Description de l'app
+        Me.Height -= 100
+        NotifyIcon.Text = "Mot du jour"
+        Titre.Text = "Bienvenue sur la super app de" + vbCrLf + "Fx & Leon"
+        Titre.TextAlign = HorizontalAlignment.Center
+        Titre.Font = New Font("Berlin Sans FB Demi", 14, FontStyle.Bold)
+        Titre.CharacterCasing = CharacterCasing.Upper
+        Button_Accept.Text = "J'aime bien votre appli, vous etes des gens biens :-)"
+        Button_quit.Text = "Nan, C'est tout nul. Ciao"
+        Button_Compliment.Text = "C"
+        Options.Text = "Opt"
+        Button_msg.Text = "Envoyer un mot d'amour"
+        CheckBoot.Text = "Lancer au demarrage de l'ordinateur"
+        CheckCompliment.Text = "Compliments (toutes les x min)"
+        Mytooltip.ShowAlways = True
+        Mytooltip.IsBalloon = True
+        Mytooltip.UseFading = True
+
+        If My.Computer.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Run\" + Application.ProductName, False) Is Nothing Then
+            CheckBoot.Checked = True
+        End If
+        If My.Settings.SetCompliment Then
+            CheckCompliment.Checked = True
+        End If
+        Destinataire.Text = "FX"
+        CreateContextMenu()
+        Randomize()
+
+        'Creation du fichier de proverbes s'il n'existe pas
+        If System.IO.File.Exists(Application.StartupPath() + "\Compliments.txt") = False Then
+            Dim Swriter As FileStream = File.Create(Application.StartupPath() + "\Compliments.txt")
+            Dim info As Byte() = New UTF8Encoding(True).GetBytes("Il faut rajouter des compliments ou recuperer le fichier de compliments aupres de FX")
+            Swriter.Write(info, 0, info.Length)
+            Swriter.Close()
+        End If
+
+
+        'Lecture et stockage des proverbes
+        Dim SReader = My.Computer.FileSystem.OpenTextFileReader(Application.StartupPath() + "\Compliments.txt")
+        While Not SReader.EndOfStream
+            SReader.ReadLine()
+            nb_compliment += 1
+        End While
+        ReDim compliment(nb_compliment)
+
+        Dim i = 0
+        SReader = My.Computer.FileSystem.OpenTextFileReader(Application.StartupPath() + "\Compliments.txt")
+        While Not SReader.EndOfStream
+            compliment(i) = SReader.ReadLine()
+            Interprete_perso(compliment(i))
+            i += 1
+        End While
+        SReader.Close()
+
+        'Lancement des compliments
+        MyTimer.Interval = 60000 * interv_comp.Text + 1
+        MyTimer.Stop()
+
+        'Lancement du client
+        'TCPListener.Start()
+        flager.Start()
+
+    End Sub
+
+
+
+    Public Sub CreateContextMenu()
+        'Define New Context Menu and Menu Item 
+        Dim contextMenu As New ContextMenu
+        Dim MenuEclair As New MenuItem("Eclair")
+        Dim MenuExit As New MenuItem("Exit")
+        contextMenu.MenuItems.Add(MenuEclair)
+        contextMenu.MenuItems.Add(MenuExit)
+
+
+
+        NotifyIcon.ContextMenu = contextMenu
+
+        'Add functionality for menu Item click 
+        AddHandler MenuEclair.Click, AddressOf MenuEclair_Click
+        AddHandler MenuExit.Click, AddressOf MenuExit_Click
+
+    End Sub
+
+    Private Sub MenuEclair_Click(ByVal sender As Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub MenuExit_Click(ByVal sender As Object, ByVal e As System.EventArgs)
+        shutup = True
+        NotifyIcon.BalloonTipText = "Au revoir"
+        NotifyIcon.ShowBalloonTip(2000)
+        Threading.Thread.Sleep(2000)
+        Me.Close()
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon.MouseDoubleClick
+        Me.Show()
+    End Sub
+
+    Private Sub Escape(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Escape Then Me.Hide()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button_Accept_Click(sender As Object, e As EventArgs) Handles Button_Accept.Click
+        Me.Hide()
+        If My.Settings.SetCompliment Then
+            MyTimer.Start()
+        End If
+    End Sub
+
+    Private Sub Button_quit_Click(sender As Object, e As EventArgs) Handles Button_quit.Click
+        Me.Hide()
+        shutup = True
+        If Not Flux Is Nothing Then
+            Flux.Close()
+        End If
+        If Not TCPClient Is Nothing Then
+            TCPClient.Close()
+        End If
+        If Not ThreadReceive Is Nothing Then
+            If ThreadReceive.IsAlive Then
+                ThreadReceive.Abort()
+            End If
+        End If
+        NotifyIcon.BalloonTipText = "A plus"
+        NotifyIcon.ShowBalloonTip(2000)
+        Threading.Thread.Sleep(2000)
+        Me.Close()
+    End Sub
+
+    Private Sub MyTimer_Tick(sender As Object, e As EventArgs) Handles MyTimer.Tick
+        Dim alea = Int(Rnd() * nb_compliment)
+        NotifyIcon.BalloonTipText = compliment(alea)
+        NotifyIcon.ShowBalloonTip(5000)
+
+    End Sub
+
+    Private Sub Button_Compliment_Click(sender As Object, e As EventArgs) Handles Button_Compliment.Click
+        Process.Start(Application.StartupPath() + "\Compliments.txt")
+    End Sub
+
+    Private Sub CheckCompliment_CheckedChanged(sender As Object, e As EventArgs) Handles CheckCompliment.CheckedChanged
+        If CheckCompliment.Checked = True Then
+            MyTimer.Start()
+            My.Settings.SetCompliment = True
+            My.Settings.Save()
+        Else
+            MyTimer.Stop()
+            My.Settings.SetCompliment = False
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub Interv_comp_ValueChanged(sender As Object, e As EventArgs) Handles interv_comp.ValueChanged
+        If interv_comp.Text = 0 Then
+            MyTimer.Interval = 1000
+        Else
+            MyTimer.Interval = 60000 * interv_comp.Text
+        End If
+    End Sub
+
+    Private Sub Options_CheckedChanged(sender As Object, e As EventArgs) Handles Options.CheckedChanged
+        If Options.Checked = True Then
+            Me.Height += 100
+        Else
+            Me.Height -= 100
+        End If
+    End Sub
+
+    Private Sub CheckBoot_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBoot.CheckedChanged
+        If CheckBoot.Checked = True Then
+            My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath)
+        Else
+            My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).DeleteValue(Application.ProductName)
+        End If
+    End Sub
+
+    Private Sub Textbox2_enter(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles TextEnvoi.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Dim f As EventArgs = New EventArgs()
+            Button_msg_Click(sender, f)
+        End If
+    End Sub
+
+
+    Private Sub Button_msg_Click(sender As Object, e As EventArgs) Handles Button_msg.Click
+        'envoi du message
+        If (EtatConnexion = 4 Or EtatConnexion = 5) Then
+            Try
+                FluxWriter.WriteLine("+d+" + Destinataire.Text + "+d+" + "Message de " + user + " :" + vbCrLf + TextEnvoi.Text + vbCrLf)
+                FluxWriter.Flush()
+                TextEnvoi.Text = ""
+                Titre.Text = "Message bien envoyé à " + Destinataire.Text
+            Catch ex As Exception
+                Titre.Text = "Envoi du Message : " + ex.Message
+                Exit Sub
+            End Try
+        Else
+            Titre.Text = "Attends que ca se connecte !"
+        End If
+    End Sub
+
+
+    Public Sub Receive_message()
+        Dim MsgRecu As String
+        While (TCPClient.Connected And Not shutup)
+            'Reception des données
+            Try
+                MsgRecu = FluxReader.ReadLine() + vbCrLf + FluxReader.ReadLine()
+            Catch ex As Exception
+                If Not shutup Then
+                    TextRecu.Invoke(SetText, New Object() {TextRecu, "Erreur reception message : " + ex.Message})
+                End If
+            End Try
+
+            'Exriture du message
+            If MsgRecu IsNot Nothing Then
+                Interprete_perso(MsgRecu)
+                If (MsgRecu IsNot "" Or MsgRecu IsNot vbCrLf) Then
+                    NotifyIcon.BalloonTipText = MsgRecu
+                    NotifyIcon.ShowBalloonTip(5000)
+                End If
+                TextRecu.Invoke(SetText, New Object() {TextRecu, MsgRecu})
+            End If
+        End While
+    End Sub
+
+
+    Private Sub Flager_Tick() Handles flager.Tick
+        'Mise a jour de l'etat de la connection
+        Solution()
+        MAJvisuel()
+
+        'envoi du flag
+        If EtatConnexion = 4 Or EtatConnexion = 5 Then
+            Try
+                FluxWriter.WriteLine(flag)
+                FluxWriter.Flush()
+                Titre.Text = "Bienvenue sur la super app de" + vbCrLf + "Fx & Leon"
+            Catch ex As Exception
+                Titre.Text = "Envoi du flag : " + ex.Message
+            End Try
+        End If
+
+        Probleme()
+    End Sub
+
+    Private Sub Solution()
+        'connexion
+        If EtatConnexion = 0 Then
+            Try
+                MdlIpAdress = IPAddress.Parse(Dns.GetHostEntry("mdlserver.ddns.net").AddressList(0).ToString())
+                EtatConnexion = 1
+            Catch ex As Exception
+                Exit Sub
+            End Try
+        End If
+
+        'Lancement du thread de connexion
+        If EtatConnexion = 1 Then
+            Try
+                ThreadConnect.Abort()
+                ThreadConnect = New Threading.Thread(AddressOf Connect_server)
+                    ThreadConnect.Start(Destinataire.Text)
+                    EtatConnexion = 2
+                Catch ex As Exception
+                Titre.Text = "Connection TCP : " + ex.Message
+                Exit Sub
+            End Try
+        End If
+
+        'Attente du thread de connexion
+        If EtatConnexion = 2 And Not ThreadConnect.IsAlive Then
+            If TCPClient.Connected Then
+                EtatConnexion = 3
+            Else
+                EtatConnexion = 1
+                Exit Sub
+            End If
+        End If
+
+        'lancement du stream
+        If EtatConnexion = 3 Then
+            Try
+                Flux = TCPClient.GetStream()
+                FluxWriter = New StreamWriter(Flux)
+                EtatConnexion = 4
+            Catch ex As Exception
+                Titre.Text = "Creation du flux : " + ex.Message + ERREUR
+                Exit Sub
+            End Try
+        End If
+        'lancement du thread de reception
+        If EtatConnexion = 4 Then
+            Try
+                FluxReader = New StreamReader(Flux)
+                ThreadReceive = New Threading.Thread(AddressOf Receive_message)
+                ThreadReceive.Start()
+                EtatConnexion = 5
+            Catch ex As Exception
+                Titre.Text = "Erreur du Thread de Reception: " & vbCrLf & ex.Message & ERREUR
+            End Try
+        End If
+    End Sub
+
+    Private Sub Probleme()
+        If EtatConnexion >= 5 Then
+            If Not ThreadReceive.IsAlive Then
+                ThreadReceive.Abort()
+                EtatConnexion = 4
+            End If
+        End If
+
+        If EtatConnexion >= 2 Then
+            If Not TCPClient.Connected And Not ThreadConnect.IsAlive Then
+                EtatConnexion = 1
+            End If
+        End If
+
+        If EtatConnexion >= 1 Then
+            Try
+                Dns.GetHostEntry("mdlserver.ddns.net")
+            Catch ex As Exception
+                EtatConnexion = 0
+            End Try
+        End If
+    End Sub
+
+    Function Connect_server(Dest As String) As Boolean
+        If Dest(0) = "1" Then
+            Try
+                TCPClient = New TcpClient(Dest, "11000")
+            Catch ex As Exception
+                Titre.Invoke(SetText, New Object() {Titre, "Erreur de connection au client. Il vaut mieux passer par la MDL." + ERREUR})
+                Thread.Sleep(5000)
+                Return False
+            End Try
+        Else
+            Try
+                TCPClient = New TcpClient(MdlIpAdress.ToString, "11000")
+            Catch ex As Exception
+                If Not shutup Then
+                    Titre.Invoke(SetText, New Object() {Titre, " Erreur de connexion au serveur MDL.   Le serveur a peut-etre un probleme. " + ERREUR})
+                    Thread.Sleep(5000)
+                End If
+                Return False
+            End Try
+        End If
+        Return True
+    End Function
+
+
+    Private Function MAJvisuel()
+        Select Case EtatConnexion
+            Case 0
+                MDLserverpic.Image = MDLserverpic.BackgroundImage
+                Mytooltip.SetToolTip(MDLserverpic, "MDLserveur introuvable")
+            Case 1
+                MDLserverpic.Image = MDLserverpic.ErrorImage
+                Mytooltip.SetToolTip(MDLserverpic, "MDLserveur trouvé, en attente de connection")
+            Case 2
+                MDLserverpic.Image = MDLserverpic.ErrorImage
+                Mytooltip.SetToolTip(MDLserverpic, "Connection")
+            Case 3
+                MDLserverpic.Image = MDLserverpic.ErrorImage
+                Mytooltip.SetToolTip(MDLserverpic, "MDl server, lien établi")
+            Case 4
+                MDLserverpic.Image = MDLserverpic.InitialImage
+                Mytooltip.SetToolTip(MDLserverpic, "Connecté")
+            Case 5
+                MDLserverpic.Image = MDLserverpic.InitialImage
+                Mytooltip.SetToolTip(MDLserverpic, "Connecté, à l'écoute")
+        End Select
+    End Function
+
+
+    Private Function Interprete_perso(ByRef S As String)
+        If S = "" Or S = vbCrLf Then
+            Exit Function
+        End If
+
+        Dim j = 0
+
+            While (j < S.Length - 1)
+            If (S.Chars(j) = "+") Then
+                Select Case (S.Chars(j + 1))
+                    Case "u"
+                        Dim temp_string = Split(S, "+u+")
+                        S = temp_string(0)
+                        For k = 1 To temp_string.Length - 1
+                            S = S + user + temp_string(k)
+                        Next
+                    Case "d"
+                        Dim temp_string = Split(S, "+d+")
+                        S = temp_string(0)
+                        For k = 2 To temp_string.Length - 1 Step 2
+                            S = S + temp_string(k)
+                        Next
+                    Case "w"
+                        S = S.Substring(2) + " s'est connecté"
+                End Select
+            End If
+            j += 1
+        End While
+        Return 0
+    End Function
+
+
+End Class
